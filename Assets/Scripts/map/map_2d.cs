f98607849b9594f27c7b8a93bffab6c30fde97d8
admin <376954839@qq.com>
2019-05-07 17:30:40 +0800

    v1.003

--- Assets/Scripts/map/map_2d.cs
+++ Assets/Scripts/map/map_2d.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -6,13 +7,39 @@ using UnityEngine.Tilemaps;
 public class map_2d : MonoBehaviour
 {
 
-    public Tilemap tilemap;//引用的Tilemap
-    public Tile baseTile;//使用的最基本的Tile，我这里是白色块，然后根据数据设置不同颜色生成不同Tile
+    public Tilemap runMap;//引用的Tilemap
+    public Tilemap wallMap; 
+    /// <summary>
+    /// 地板Tile
+    /// </summary>
+    public Tile baseTile;
+    /// <summary>
+    /// 资源tile
+    /// </summary>
+    public Tile[] resourcesTiles;
+    /// <summary>
+    /// 墙tile
+    /// </summary>
+    public Tile wallTile;
+
+    public Vector3Int startTile;
     Tile[] arrTiles;//生成的Tile数组
+
+    int[,] map;
+    [Range(0, 100)] //这行代码用来在面板上显示一个滑动条
+    public int probability;
+//地图的长和高
+public int width=8;
+    public int height=8;
+    //种子
+    public string seed;
+    //是否要使用随机种子
+    public bool useRandomSeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateMap();
     }
 
     // Update is called once per frame
@@ -21,44 +48,40 @@ public class map_2d : MonoBehaviour
         
     }
 
-    void mapChange() {
-        //销毁墙体
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mousePosition = Input.mousePosition;
-            Vector3 wordPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            Vector3Int cellPosition = tilemap.WorldToCell(wordPosition);
-            //tilemap.SetTile(cellPosition, gameUI.GetSelectColor().colorData.mTile);
-            TileBase tb = tilemap.GetTile(cellPosition);
-            if (tb == null)
-            {
-                return;
-            }
-            //tb.hideFlags = HideFlags.None;
-            Debug.Log("鼠标坐标" + mousePosition + "世界" + wordPosition + "cell" + cellPosition + "tb" + tb.name);
-            //某个地方设置为空，就是把那个地方小格子销毁了
-            tilemap.SetTile(cellPosition, null);
-            //tilemap.RefreshAllTiles();
-        }
+    //空白地方创造墙体
+    void DrawTile(Vector3 vector, Tile tile) {
+       // Vector3 mousePosition = Input.mousePosition;
+       // Vector3 wordPosition = Camera.main.ScreenToWorldPoint(vector);
+        Vector3Int cellPosition = runMap.WorldToCell(vector);
+        //tilemap.SetTile(cellPosition, gameUI.GetSelectColor().colorData.mTile);
+        //TileBase tb = tilemap.GetTile(cellPosition);
+        //if (tb != null)
+        //{
+        //    return;
+        //}
+        //tb.hideFlags = HideFlags.None;
+        //Debug.Log("鼠标坐标" + mousePosition + "世界" + wordPosition + "cell" + cellPosition + "tb" + tb.name);
+        //格子填充
+        runMap.SetTile(cellPosition, tile);
+        //tilemap.RefreshAllTiles();
 
-        //空白地方创造墙体
-        if (Input.GetMouseButtonDown(1))
+    }
+    //销毁墙体
+    void RemoveTile(Vector3 vector) {
+       // Vector3 mousePosition = Input.mousePosition;
+        Vector3 wordPosition = Camera.main.ScreenToWorldPoint(vector);
+        Vector3Int cellPosition = runMap.WorldToCell(wordPosition);
+        //tilemap.SetTile(cellPosition, gameUI.GetSelectColor().colorData.mTile);
+        TileBase tb = runMap.GetTile(cellPosition);
+        if (tb == null)
         {
-            Vector3 mousePosition = Input.mousePosition;
-            Vector3 wordPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            Vector3Int cellPosition = tilemap.WorldToCell(wordPosition);
-            //tilemap.SetTile(cellPosition, gameUI.GetSelectColor().colorData.mTile);
-            TileBase tb = tilemap.GetTile(cellPosition);
-            if (tb != null)
-            {
-                return;
-            }
-            //tb.hideFlags = HideFlags.None;
-            //Debug.Log("鼠标坐标" + mousePosition + "世界" + wordPosition + "cell" + cellPosition + "tb" + tb.name);
-            //格子填充
-            tilemap.SetTile(cellPosition, baseTile);
-            //tilemap.RefreshAllTiles();
+            return;
         }
+        //tb.hideFlags = HideFlags.None;
+        Debug.Log( "世界" + wordPosition + "cell" + cellPosition + "tb" + tb.name);
+        //某个地方设置为空，就是把那个地方小格子销毁了
+        runMap.SetTile(cellPosition, null);
+        //tilemap.RefreshAllTiles();
     }
 
 
@@ -78,14 +101,14 @@ public class map_2d : MonoBehaviour
         {
             //想做生命墙，需要自己做个数据层，对应索引id就行
             arrTiles[i] = ScriptableObject.CreateInstance<Tile>();//创建Tile，注意，要使用这种方式
-            arrTiles[i].sprite = baseTile.sprite;
-            arrTiles[i].color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1);
+          //  arrTiles[i].sprite = baseTile.sprite;
+            arrTiles[i].color = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), 1);
         }
         for (int i = 0; i < levelH; i++)
         {//这里就是设置每个Tile的信息了
             for (int j = 0; j < levelW; j++)
             {
-                tilemap.SetTile(new Vector3Int(j, i, 0), arrTiles[Random.Range(0, arrTiles.Length)]);
+                runMap.SetTile(new Vector3Int(j, i, 0), arrTiles[UnityEngine.Random.Range(0, arrTiles.Length)]);
             }
             yield return null;
         }
@@ -97,9 +120,107 @@ public class map_2d : MonoBehaviour
             // arrTiles[colorIdx].color = new Color(Random.Range(0f, 1f), Random.Range(0f,1f), Random.Range(0f, 1f), 1);
             // tilemap.RefreshAllTiles();
 
-            Color c = tilemap.color;//这里是改变Tilemap的颜色，尝试是否可以整体变色
+            Color c = runMap.color;//这里是改变Tilemap的颜色，尝试是否可以整体变色
             c.a -= Time.deltaTime;
-            tilemap.color = c;
+            runMap.color = c;
+        }
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();    //随机生成地图
+
+        for (int i = 0; i < 2; i++)
+        {
+            SmoothMap();
+        }
+        DrawMap();
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+            seed = DateTime.Now.ToString();
+
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1)  //边缘是墙
+                    map[i, j] = 1;
+                else
+                    map[i, j] = (pseudoRandom.Next(0, 100) < probability) ? 1 : 0;  //1是墙，0是空地
+            }
+        }
+       
+
+    }
+
+
+    void DrawMap()
+    {
+        if (map != null)
+        {
+            map[startTile.x, startTile.y] = 0;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    EventManage.Instance.LogWrap(map[i, j].ToString());
+                    if (map[i, j] == 0)
+                    {
+                        runMap.SetTile(new Vector3Int(j, i, 0),baseTile );
+                    }
+                    else
+                    {
+                        wallMap.SetTile(new Vector3Int(j, i, 0), wallTile);
+                    }
+                  
+                }
+            }
+        }
+    }
+
+
+//**去噪点
+    int GetSurroundingWalls(int posX, int posY)
+    {
+        int wallCount = 0;
+
+        for (int i = posX - 1; i <= posX + 1; i++)
+        {
+            for (int j = posY - 1; j <= posY + 1; j++)
+            {
+                if (i >= 0 && i < width && j >= 0 && j < height)
+                {
+                    if (i != posX || j != posY)
+                        wallCount += map[i, j];
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+    void SmoothMap()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                int surroundingTiles = GetSurroundingWalls(i, j);
+
+                if (surroundingTiles > 4)
+                    map[i, j] = 1;
+                else if (surroundingTiles < 4)
+                    map[i, j] = 0;
+            }
         }
     }
 }
