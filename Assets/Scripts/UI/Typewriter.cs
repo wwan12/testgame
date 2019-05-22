23072bb4e9e8455fcbae9eacbd0131c25d674257
admin <376954839@qq.com>
2019-05-22 16:41:33 +0800

    v1.0010

---
+++ Assets/Scripts/UI/Typewriter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typewriter : MonoBehaviour
+{
+    [Tooltip("显示文字的间隔")]
+    public float pause=0.1f;
+    [Tooltip("缓存数据条数")]
+    public int cacheNum=20;
+    [Tooltip("自适应")]
+    public bool isAdaption;
+    private string baseText;
+    private ScrollRect scrollRect;
+    private Queue<string> queue;
+    private Text textUI;
+    private string printText;//打印的字
+    private bool isRun;
+    private int lines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollRect= gameObject.GetComponentInParent<ScrollRect>();
+
+        textUI = gameObject.GetComponent<Text>();
+        queue = new Queue<string>(cacheNum);
+
+        if (isAdaption)
+        {
+            gameObject.AddComponent<ContentSizeFitter>().verticalFit= ContentSizeFitter.FitMode.PreferredSize;
+        }
+        else
+        {
+             lines = Convert.ToInt32((textUI.rectTransform.sizeDelta.y-10) / (textUI.fontSize + textUI.lineSpacing*2));
+            
+        }
+        
+        for (int i = 0; i < 14; i++)
+        {
+            AddQueue(i.ToString()+":abcd");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitBaseText() {
+        AddQueue("据上次启动已经过" + Time.time + "次时钟频率");
+        AddQueue("正在接管系统日志...");
+        AddQueue("系统已上线_");
+    }
+
+    public void AddQueue(string text)
+    {        
+        queue.Enqueue(text);
+        if (!isRun)
+        {
+            isRun = true;
+            StartCoroutine(TypeText());
+        }         
+    }
+
+    public void Clear()
+    {
+        textUI.text = "";
+        AddQueue("系统清除日志");
+    }
+
+    public void Hide() {
+
+    }
+    public void Show()
+    {
+
+    }
+
+    /**输出文本功能*/
+    IEnumerator TypeText()
+    {
+        while (queue.Count>0)
+        {
+            string mText = "";
+            if (!isAdaption)//非自适应自动删除第一条
+            {
+                string[] c = textUI.text.Split('\n');
+                if (c.Length >= lines)
+                {
+                    c[0] = "";
+                    for (int i = 1; i < c.Length; i++)
+                    {
+                        mText += (c[i] + "\n");
+                    }
+                    Debug.Log(mText);
+                }
+                else
+                {
+                    mText = textUI.text;
+                }
+            }
+          
+            string cache = isAdaption?textUI.text.ToString():mText;
+            string word = queue.Dequeue();
+            foreach (char letter in word.ToCharArray())
+            {
+                printText += letter;//把这些字赋值给Text
+                yield return new WaitForSeconds(pause);
+                textUI.text = cache+printText;
+            }
+            textUI.text = textUI.text + "\n";
+            printText = "";
+            scrollRect.verticalNormalizedPosition = 0;
+            yield return new WaitForSeconds(3*pause);
+           
+        }
+        isRun = false;
+        //显示完成换行
+    }
+}
